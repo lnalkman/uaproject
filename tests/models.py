89385bee72de06c82fbd456cafccbfaf9a56a085
Oleksandr Kravchuk <lnalkman@gmail.com>
2017-03-30 11:49:51 +0300

    Додав модель тесту в БД та панель адміністратора

--- tests/models.py
+++ tests/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Test(models.Model):
+    # Умова завдання
+    condition = models.TextField(verbose_name='Умова завдання', max_length=512, null=True)
+
+    # Варіанти відповідей
+    first = models.CharField(verbose_name='а) :', max_length=128, null=True)
+    second = models.CharField(verbose_name='б) :', max_length=128, null=True)
+    third = models.CharField(verbose_name='в) :', max_length=128, null=True)
+    fourth = models.CharField(verbose_name='г) :', max_length=128, null=True)
+
+    # Вибір правильного варіанту
+    variants = (
+        (0, "Варіант а)"),
+        (1, "Варіант б)"),
+        (2, "Варіант в)"),
+        (3, "Варіант г)"),
+    )
+    correctChoice = models.PositiveSmallIntegerField(verbose_name='Правильна відповідь:', choices=variants, null=True)
+
+    # Вивід лише правильного варіанту
+    def __str__(self):
+        answers = [self.first, self.second, self.third, self.fourth]
+        choices = ['а)', 'б)', 'в)', 'г)']
+        result = 'NULL'
+        choice = 'NULL'
+        try:
+            result = answers[int(self.correctChoice)]
+            choice = choices[int(self.correctChoice)]
+        except IndexError:
+            pass
+
+        return choice + ' ' + result
