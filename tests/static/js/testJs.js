8dee92ae32a3010524a1984ad45b7d06734d2047
Oleksandr <lnalkman@gmail.com>
2017-04-05 18:19:33 +0300

    Зробив заготовку для классу тестувальної машини

---
+++ tests/static/js/testJs.js
@@ -0,0 +1,47 @@
+"use strict"
+
+function TestingMachine() {
+
+  var activeQuestion = 0;
+  var realQuestionList = [];
+  var answerList = {};
+
+  function timeoutErrorHandler() {
+
+  };
+
+  function choosingQuestionErrorHandler() {
+
+  };
+
+  function sendAnswers() {
+
+  };
+
+  function getQuestions() {
+
+  };
+
+  function generateQuestList() {
+
+  };
+
+  function chooseQuestion() {
+
+  };
+
+  this.reply = function () {
+
+  };
+
+  this.pass = function () {
+
+  };
+
+  this.showQuestionList = function () {
+
+  };
+
+
+
+};
